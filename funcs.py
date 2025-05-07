9124216f87a98f6a18a732ea0e1418efa0decf45
Marcelbrn <marcelbrn@gmail.com>
2025-05-07 10:04:26 -0300

    Adicionando função para estilo css

--- funcs.py
+++ funcs.py
@@ -1,1 +1,7 @@
-.
+# ===== Importação bibliotecas ===== #
+import streamlit as st
+
+# ===== Definindo função para gerar estilo css personalizado da página ===== #
+def load_css(v_path_css):
+    with open(v_path_css) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
