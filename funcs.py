0e8b05fd9e04c0adeac704a0eafac2835de10623
Marcelbrn <marcelbrn@gmail.com>
2025-05-12 20:24:30 -0300

    Ajuste de publicação

--- funcs.py
+++ funcs.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 # ===== Definindo função para gerar estilo css personalizado da página ===== #
 def load_css(v_path_css):
-    with open(v_path_css) as f:
+    with open(v_path_css, "r", encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
         
 
