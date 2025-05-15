c25e3e10b567d4e5f0da9676f18c49b630f5781e
Marcelbrn <marcelbrn@gmail.com>
2025-05-15 08:27:55 -0300

    Ajuste de publicação

--- funcs.py
+++ funcs.py
@@ -49,7 +49,8 @@ def projects(v_projetos):
 # ===== Definindo função para gerar quadro de certificações ===== #
 def certification(v_certificacoes):
     st.markdown("### 🏅 Certificações")
-    cols = st.columns(4, gap="small")
+    #cols = st.columns(4, gap="small")
+    cols = st.columns([0.20, 0.20, 0.20, 0.20])
     for idx, cert in enumerate(v_certificacoes):
         with cols[idx % 4]:
             st.markdown(
